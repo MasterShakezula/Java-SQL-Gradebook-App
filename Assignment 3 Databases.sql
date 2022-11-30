4263ad65acc219bd31a2e42119fb01807d9bddc3
MasterShakezula <90061079+MasterShakezula@users.noreply.github.com>
2022-11-29 20:55:17 -0500

    Part 2 done (Triggers)
    Procedures/Triggers added to update and set letter grades based on points and grading scale

--- Assignment 3 Databases.sql
+++ Assignment 3 Databases.sql
@@ -5,50 +5,43 @@ CREATE USER 'gradebook-admin' IDENTIFIED BY 'Grad3B00k!';
 GRANT ALL ON gradebook to 'gradebook-admin';
 
 -- Part 2 Triggers
-DELIMITER $$
-CREATE TRIGGER check_grade BEFORE UPDATE ON ENROLLMENT
-FOR EACH ROW 
-BEGIN
-        IF  EXISTS (SELECT * FROM ENROLLMENT WHERE NEW.Total_Points > 100) THEN
-               signal sqlstate '45000' set message_text = 'Grade cannot be more than 100';
-       END IF;
-       
-END; $$
-DELIMITER ;
-drop trigger update_letter_grade;
+
 use gradebook;
 DELIMITER $$
-CREATE TRIGGER update_letter_grade BEFORE UPDATE ON ENROLLMENT
-FOR EACH ROW 
-BEGIN
-				select A_Min, B_Min, C_Min, D_Min from course, enrollment where Course_Id = course.Course;
-       
-               update enrollment set new.Final_Grade = 'A' where new.Total_Points >= course.A.Min;
-		
-               update enrollment set Final_Grade = 'B' where new.Total_Points >= B.Min;
-		
-               update enrollment set Final_Grade = 'C' where new.Total_Points >= C.Min;
-		
-               update enrollment set Final_Grade = 'D' where new.Total_Points >= D.Min;
-		
-               update enrollment set Final_Grade = 'F' where new.Total_Points < D.Min;
-END;$$
+create procedure `Calculate_Grades` (in course_id varchar(20) )
+begin
+declare letter_grade char;
+declare A_Min float; declare B_Min float; declare C_Min float; declare D_Min float; declare grade float;
+declare stu varchar(10);
+declare sect int;
+declare finished integer default 0;
+declare students_cursor cursor for
+select e.Total_Points, e.Section_No, e.Student, c.A_Min, c.B_Min, c.C_Min, c.D_Min from enrollment e
+inner join section s on e.Section_No = s.Section_No 
+inner join course c on s.Course = c.Course_Id 
+where s.Course = Course_Id; --
+declare continue handler for not found set finished = 1;
+start transaction;
+open students_cursor;
+std_loop : loop  fetch students_cursor into stu, sect, A_Min, B_Min, C_Min, D_Min, grade;
+if finished = 1 then leave std_loop;
+end if;
+			if (grade >= A_Min) then
+				set letter_grade = 'A';
+			elseif (grade >= B_Min) then
+				set letter_grade = 'B';
+			elseif (grade >= C_Min) then
+				set letter_grade = 'C';
+			elseif (grade >= D_Min) then
+				set letter_grade = 'D';
+                end if;
+                update enrollment set Final_Grade = letter_grade where Student = stu and section_No = sect;
+                end loop std_loop;
+                close students_cursor;
+
+
+END$$;
+
 DELIMITER ;
-update enrollment
-set Total_Points = 80
-where Student = '1234561';
--- delete from enrollment where Student = '1234560';
-
-
-DELIMITER $$
-CREATE TRIGGER check_grade_ins BEFORE INSERT ON ENROLLMENT
-FOR EACH ROW 
-BEGIN
-        IF  EXISTS (SELECT * FROM ENROLLMENT WHERE NEW.Total_Points > 100) THEN
-               signal sqlstate '45000' set message_text = 'Grade cannot be more than 100';
-       END IF;
-       
-END; $$
-DELIMITER ;
-
+call Calculate_Grades('BME-4050L');
 
