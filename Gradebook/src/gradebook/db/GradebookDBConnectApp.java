6dcb1cd5abdb36184a591fad81e891fd5223565a
MasterShakezula <90061079+MasterShakezula@users.noreply.github.com>
2022-11-30 15:58:49 -0500

    Integrated Java Program
    Updated java program to be able to connect to the gradebook database and either view a list of students and their grades in a specific function or a list of sections of which a student is enrolled in, along with their grade. Queries will possibly need an update to work properly.

--- Gradebook/src/gradebook/db/GradebookDBConnectApp.java
+++ Gradebook/src/gradebook/db/GradebookDBConnectApp.java
@@ -17,30 +17,45 @@ public class GradebookDBConnectApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 String username = "gradebook-admin";
-		    String password = "Grad3B00k!";
-		    String dbName = "gradebook";
-		    String hostname = "localhost";
-		    Connection conn = null;
-		try {
-		   StringBuffer connectionString = new StringBuffer("jdbc:mysql://");
-		   connectionString.append(hostname+"/");
-		   connectionString.append(dbName);
-		   connectionString.append("?user="+username);
-		    connectionString.append("&password="+password);
-		    System.out.println("Connection String: "+connectionString.toString());
-		   
-		    conn = DriverManager.getConnection(connectionString.toString());
-		   
-		    if(conn != null)
-		        System.out.println("Connected to DB !!");
-		} 
-		catch (Exception ex) { System.out.println("SQLException: " + ex.getMessage()); }
-		finally {
-		try { conn.close();}
-		 catch (SQLException e) { e.printStackTrace();  }
+		
+		if (args.length == 0)
+		{
+			System.out.println("Please provide a valid section or student");
+			System.exit(0);
+		}
+		String input = args[0];
+		DBConnection dbConn = new DBConnection();
+		try
+		{
+			boolean inputFound = false;
+			dbConn.openConnection();
+			inputFound = dbConn.getStudentEnrollment(input);
+			if (!inputFound) inputFound = dbConn.getSectionEnrollment(input);
+			if (!inputFound) System.out.println(input + " is not a valid section or student");
+				
+		}
+		finally
+		{
+			dbConn.closeConnection();
+		}
+		/* Old-code. Keep just in case
+		 * // TODO Auto-generated method stub String username = "gradebook-admin";
+		 * String password = "Grad3B00k!"; String dbName = "gradebook"; String hostname
+		 * = "localhost"; Connection conn = null; try { StringBuffer connectionString =
+		 * new StringBuffer("jdbc:mysql://"); connectionString.append(hostname+"/");
+		 * connectionString.append(dbName); connectionString.append("?user="+username);
+		 * connectionString.append("&password="+password);
+		 * System.out.println("Connection String: "+connectionString.toString());
+		 * 
+		 * conn = DriverManager.getConnection(connectionString.toString());
+		 * 
+		 * if(conn != null) System.out.println("Connected to DB !!"); } catch (Exception
+		 * ex) { System.out.println("SQLException: " + ex.getMessage()); } finally { try
+		 * { conn.close();} catch (SQLException e) { e.printStackTrace(); }
+		 * 
+		 * }
+		 */
 
+}
 	}
 
-}}
